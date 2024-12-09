2db67e8682518c8719ce1852306955ef278108d8
Hoàng Anh Quân <quan.hoang@gihot.vn>
2024-12-09 18:32:20 +0700

    fix bug

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -1,7 +1,5 @@
+
 using System.Collections;
-using System.Collections.Generic;
-using System.Net.WebSockets;
-using SingularityGroup.HotReload;
 using UnityEngine;
 
 
@@ -11,65 +9,32 @@ public class PlayerData
     public float MoveSpeed = 5;
 
 }
+
+public enum PlayerState { Idle = 0, Walk=1, Run=2, Jump=3, Climb=6, Falling=4, Landing=5 }
 public class PlayerController : MonoBehaviour
 {
-    CharacterController m_CharacterController;
+    CharacterController m_Controller;
     Animator m_animator;
-    [Range(5, 15)]
+    [Range(4, 15)]
     public float MaxSpeed = 5;
     public float RotationSpeed = 15;
     public float Acceleration = 20;
-    public float MaxJumpSpeed = 0;
+    public float JumpForce = 5;
     public bool Grounded = false;
     Vector2 velocity = Vector2.zero;
 
     float JumpTime = 0;
-    float jumpSpeed = 0;
-    [HideInInspector]
-    public Vector3 detalMove = Vector3.zero;
-    public float Gravity = -2;
+    public PlayerState playerState = PlayerState.Idle;
+    public float GravityScale = 1;
+    public float Gravity { get { return Physics.gravity.y * GravityScale; } }
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        m_CharacterController = GetComponent<CharacterController>();
+        m_Controller = GetComponent<CharacterController>();
         m_animator = GetComponent<Animator>();
     }
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(transform.position, 0.2f);
-    }
-    void HandleVertical()
-    {
 
-    }
-    private void OnGUI()
-    {
-        GUIStyle style = new GUIStyle();
-        style.fontSize = 24;
-        GUI.Label(new Rect(0, 0, 300, 24), "RunSpeed: " + velocity.x, style);
-        GUI.Label(new Rect(0, 24, 300, 24), "IsGrounded: " + Grounded, style);
-    }
-
-    void CalculateRunSpeed(Vector3 movement)
-    {
-        float scale = 1;
-        if (movement.magnitude <= 0)
-        {
-            scale = -1;
-        }
-        velocity.x += Acceleration * scale * Time.deltaTime;
-        velocity.x = Mathf.Clamp(velocity.x, 0, MaxSpeed);
-    }
-
-    private void GroundedCheck()
-    {
-        // set sphere position, with offset
-        Grounded = Physics.CheckSphere(transform.position+Vector3.up*0.1f, 0.2f, 1, QueryTriggerInteraction.Ignore);
-
-    }
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         GroundedCheck();
 
@@ -90,71 +55,139 @@ public class PlayerController : MonoBehaviour
         }
 
         CalculateRunSpeed(movement);
+        
 
         m_animator.SetFloat("speed", velocity.x);
         if (Input.GetKeyUp(KeyCode.V))
         {
             m_animator.SetTrigger("turn180");
         }
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            m_animator.SetBool("isJumping", true);
-            m_animator.SetBool("isFalling", false);
-
-            JumpTime = 0;
-            velocity.y = MaxJumpSpeed;
-        }
-
-        JumpTime += Time.deltaTime;
-
-
-        if (velocity.y < 0 && !Grounded)
-        {
-            m_animator.SetBool("isJumping", false);
-            m_animator.SetBool("isFalling", true);
-        }
-        // if(isGrounded)
-        // {
-        //     m_animator.SetBool("isJumping", false);
-        //     m_animator.SetBool("isFalling", false);
-        // }
-        m_animator.SetBool("isGrounded", Grounded);
-
-
-    }
-
-
-    void FixedUpdate()
-    {
-        HandleFalling();
-    }
-
-    void HandleFalling()
-    {
-        velocity.y = velocity.y + Gravity * JumpTime;
         if(Grounded)
         {
-            velocity.y = -2;
+            if (velocity.y < 0)
+            {
+                velocity.y = -2;
+            }
         }
         else
         {
-            velocity.y = Mathf.Clamp(jumpSpeed, Gravity, -Gravity);
+            velocity.y += Gravity * Time.deltaTime;
         }
+        JumpTime += Time.deltaTime;
+
+    
+        UpdateAnyState();
+        UpdateStateMachine();
+        m_animator.SetInteger("playerState", (int)playerState);
+
+        m_animator.SetBool("isGrounded", Grounded);
+        Move();
 
     }
+    void UpdateAnyState()
+    {
+        if (Input.GetKeyUp(KeyCode.Space) && Grounded)
+        {
+            playerState = PlayerState.Jump;
+            StartCoroutine(Jump());
+            JumpTime = 0;
+        }
+      
+    }
+    void UpdateStateMachine()
+    {
+        switch (playerState)
+        {
+            case PlayerState.Jump:
+                if (velocity.y < 0 && JumpTime > 0.8)
+                {
+                    playerState = PlayerState.Falling;
+                }
+                break;
+            case PlayerState.Falling:
+                if (Grounded)
+                {
+                    playerState = PlayerState.Landing;
+                }
+                break;
+            case PlayerState.Landing:
+                // playerState = PlayerState.Idle;
+                break;
+            case PlayerState.Idle:
+                break;
+        }
+    }
+    IEnumerator Idle(float Delay)
+    {
+        yield return new WaitForSeconds(Delay);
+        playerState = PlayerState.Idle;
+    }
+    IEnumerator Jump()
+    {
+        yield return new WaitForSeconds(0.16f);
+        // velocity.y = JumpForce;
+        
+    }
+    private void Move()
+    {
+        m_Controller.Move( Vector3.up * velocity.y * Time.deltaTime);
+        // m_Controller.Move(transform.forward * velocity.x / 2 * Time.deltaTime);
+    }
+    private void FixedUpdate()
+    {
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, 0.2f);
+    }
+    private void HandleVertical()
+    {
+
+    }
+    private void OnGUI()
+    {
+        GUIStyle style = new GUIStyle();
+        style.fontSize = 24;
+        GUI.Label(new Rect(0, 0, 300, 24), "RunSpeed: " + velocity.x, style);
+        GUI.Label(new Rect(0, 24, 300, 24), "IsGrounded: " + Grounded, style);
+        GUI.Label(new Rect(0, 48, 300, 24), "Velocity: " + velocity, style);
+    }
+
+    private void CalculateRunSpeed(Vector3 movement)
+    {
+        float scale = 1;
+        if (movement.magnitude <= 0)
+        {
+            scale = -1;
+        }
+        velocity.x += Acceleration * scale * Time.deltaTime;
+        velocity.x = Mathf.Clamp(velocity.x, 0, MaxSpeed);
+    }
+
+    private void GroundedCheck()
+    {
+        // set sphere position, with offset
+        Grounded = Physics.CheckSphere(transform.TransformPoint(m_Controller.center) + Vector3.down * m_Controller.height/2, m_Controller.radius, 1, QueryTriggerInteraction.Ignore);
+
+    }
+    // Update is called once per frame
 
 
 
-    void OnAnimatorMove()
+
+    float starttime = 0;
+    float distance = 0;
+    private void OnAnimatorMove()
     {
         Vector3 move = m_animator.deltaPosition;
-        move += detalMove;
+        Debug.Log("Velocity:" + move.magnitude / Time.deltaTime);
         if (!Grounded)
         {
-            move = velocity * MaxSpeed * Time.deltaTime / 2;
+            move = velocity.x/2 * transform.forward * Time.deltaTime;
         }
-        move.y = velocity.y * Time.deltaTime;
-        m_CharacterController.Move(move);
+            move.y = 0;
+        m_Controller.Move(move);
     }
 
 }
---
+++ Assets/Scripts/Characters/PlayerInput.cs
@@ -0,0 +1,28 @@
+// using UnityEngine;
+
+// public class PlayerInput
+// {
+//     public Vector3 MoveInput;
+//     public bool isJumping;
+//     public bool isSprinting;
+//     public bool isCrouching;
+//     public bool isInteracting;
+//     public bool isAiming;
+//     public bool isReloading;
+//     public bool isRunning;
+//     public bool isWalking;
+//     public bool isClimbing;
+//     public bool isClimbingUp;
+//     public bool isClimbingDown;
+//     public bool isClimbingLeft;
+//     public bool isClimbingRight;
+
+
+//     /// <summary>
+//     /// Update is called every frame, if the MonoBehaviour is enabled.
+//     /// </summary>
+//     private void Update()
+//     {
+        
+//     }
+// }
